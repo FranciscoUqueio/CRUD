250ab0a9c9bf2dc3b2c479364090a9276c449730
FranciscoUqueio <dev.franciscouqueio.jr@gmail.com>
2022-11-10 11:19:42 +0200

    feat: list all users feature

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,25 @@
 using Aplication.Dtos;
+using Aplication.Interfaces;
 using Aplication.Users;
 using Doiman;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class UsersController: BaseApiController
 {
     private readonly IMediator _mediator;
+    private readonly UserManager<User> _userManager;
+    private readonly IUserNamesList _userNamesList;
 
-    public UsersController(IMediator mediator)
+    public UsersController(IMediator mediator, UserManager<User> userManager, IUserNamesList userNamesList)
     {
         _mediator = mediator;
+        _userManager = userManager;
+        _userNamesList = userNamesList;
     }
 
     [HttpPost]
@@ -20,17 +27,31 @@ public class UsersController: BaseApiController
     {
         return await _mediator.Send(command);
     }
+
+    [HttpGet]
+    public async Task<ActionResult<List<UserDto>>> GetAllUsers()
+    {
+        return await _mediator.Send(new ListUsers.ListUsersQuery());
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUserById(string id)
+    {
+        return await _mediator.Send(new ListUserById.ListUserByIdQuery{Id = id});
+    }
+
+    [HttpGet("usernames")]
+    public async Task<ActionResult<List<string>>> GetUsernames()
+    {
+        return await _mediator.Send(new UsernameList.UsernameListQuery());
+    }
+
+    [HttpGet("users")]
+    public async Task<ActionResult<List<string>>> GetUserNames2()
+    {
+        var userList = await _userManager.Users.ToListAsync();
+        return _userNamesList.GetUserNames(userList);
+    }
+
 
 }
---
+++ Aplication/Users/ListUsers.cs
@@ -0,0 +1,33 @@
+using Aplication.Dtos;
+using AutoMapper;
+using Doiman;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplication.Users;
+
+public class ListUsers
+{
+    public class ListUsersQuery: IRequest<List<UserDto>>
+    {
+    }
+    
+    public class ListUsersHandler: IRequestHandler<ListUsersQuery, List<UserDto>>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+        public ListUsersHandler(UserManager<User> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        public async Task<List<UserDto>> Handle(ListUsersQuery request, CancellationToken cancellationToken)
+        {
+            var usersList = await _userManager.Users.ToListAsync(cancellationToken);
+
+            return _mapper.Map<List<UserDto>>(usersList);
+        }
+    }
+}
