e9115b9a7de5fda03334db457dccb40735baf2c5
FranciscoUqueio <dev.franciscouqueio.jr@gmail.com>
2022-11-10 16:47:08 +0200

    feat: implemented Token Service

---
+++ Infrastruture/Services/TokenService.cs
@@ -0,0 +1,47 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Aplication.Interfaces;
+using Doiman;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastruture.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _configuration;
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:key"]));
+    }
+
+    public string CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id),
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.GivenName, user.Fullname),
+            new("UserName", user.UserName)
+        };
+        var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = credentials,
+            Issuer = _configuration["Token:Issuer"]
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(token);
+    }
+}
