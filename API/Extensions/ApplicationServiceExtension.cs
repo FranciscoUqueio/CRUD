82d43525690855f33afc57c6f1937d922f881271
FranciscoUqueio <dev.franciscouqueio.jr@gmail.com>
2022-11-10 16:49:52 +0200

    feat: implemented token service and Repository pattern

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -8,6 +8,8 @@ public static class ApplicationServiceExtension
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IUserNamesList, UserList>();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPostRepository, PostRepository>();
         return services;
     }
 }
